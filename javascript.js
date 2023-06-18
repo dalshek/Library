b739cc4e5097ce2d81d1eb9cb37ff7b0b415cab0
dalshek <cdiezpag@gmail.com>
2023-06-18 20:04:14 +0200

    Adding manual content to the DOM

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ let myLibrary = [];
 const theHobbit = new Books("The Hobbit", "J.R.R. Tolkien", 295, false);
 const HP = new Books("HP", "J.K. Rowling", 432, true)
 const LOTR = new Books("LOTR", "J.R.R. Tolkien", 578, true)
+const bookClass = document.querySelector('.book');
 
 
 //--------------------CONSTRUCTOR---------------------------
@@ -22,7 +23,20 @@ function addBookToLibrary(book) {
 }
 function bookLoop(){
     myLibrary.forEach(function(book){
-        console.log(book.title)
+        //console.log(book.title)
+        const tr = document.createElement('tr');
+        const td1 = document.createElement('td');
+        const td2 = document.createElement('td');
+        const td3 = document.createElement('td');
+        td1.textContent = book.title;
+        tr.appendChild(td1);
+        td2.textContent = book.author;
+        tr.appendChild(td2);
+        td3.textContent = book.read;
+        tr.appendChild(td3);
+        bookClass.appendChild(tr);
+
+
     })
 }
 
