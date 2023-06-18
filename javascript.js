dcbdf2650eab9698a283fa0266dfbf7998b2844b
dalshek <cdiezpag@gmail.com>
2023-06-18 18:52:02 +0200

    Showing function created (console), adding content to the DOM

--- javascript.js
+++ javascript.js
@@ -1,31 +1,35 @@
+//-------------------INITIALIZING VARIABLES--------------------
+let myLibrary = [];
+const theHobbit = new Books("The Hobbit", "J.R.R. Tolkien", 295, false);
+const HP = new Books("HP", "J.K. Rowling", 432, true)
+const LOTR = new Books("LOTR", "J.R.R. Tolkien", 578, true)
+
+
+//--------------------CONSTRUCTOR---------------------------
 function Books(title, author, pages, read){
-    title,
-    author,
-    pages,
-    read
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read
     this.info = function() {
         return title + " by " + author + ", " + pages + ", " + (read  ? "read" : "not read yet")
     }
 }
 
-let myLibrary = [];
-
+//---------------------FUNCTIONS-------------------------------
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
-
-const theHobbit = new Books("The Hobbit", "J.R.R. Tolkien", 295, false);
-const HP = new Books("HP", "J.K. Rowling", 432, true)
-const LOTR = new Books("LOTR", "J.R.R. Tolkien", 578, true)
-
 function bookLoop(){
-    for(let book in myLibrary){
-        console.log(book);
-    }
+    myLibrary.forEach(function(book){
+        console.log(book.title)
+    })
 }
 
 
+//----------------------CALL---------------------
 addBookToLibrary(theHobbit);
 addBookToLibrary(HP);
 addBookToLibrary(LOTR);
 bookLoop();
+
